b902a73156bc53abea18759945d5b175056e8c49
raghunayyar <me@iraghu.com>
2013-06-19 18:14:47 +0530

    Removes undefined Controller.

--- settings/controller/users/pagecontroller.php
+++ settings/controller/users/pagecontroller.php
@@ -4,9 +4,8 @@
  * This file is licensed under the Affero General Public License version 3 or later.
  * See the COPYING-README file.
  */
-namespace OCP\Settings\Users\Controller;
 
-class Users_PageController extends Controller {
+class Users_PageController {
 	public function index() {
 		$this->render('users/main');
 	}
