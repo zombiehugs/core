f1ac5e6b8149216bd6ff34041032cdf5edd29c76
Robin Appelman <icewind@owncloud.com>
2013-07-01 17:41:49 +0200

    remove unneeded delTree

--- lib/files/storage/local.php
+++ lib/files/storage/local.php
@@ -155,7 +155,7 @@ if (\OC_Util::runningOnWindows()) {
 		}
 
 		public function unlink($path) {
-			return $this->delTree($path);
+			return unlink($this->datadir . $path);
 		}
 
 		public function rename($path1, $path2) {
@@ -209,27 +209,6 @@ if (\OC_Util::runningOnWindows()) {
 			} else {
 				return false;
 			}
-		}
-
-		private function delTree($dir) {
-			$dirRelative = $dir;
-			$dir = $this->datadir . $dir;
-			if (!file_exists($dir)) return true;
-			if (!is_dir($dir) || is_link($dir)) return unlink($dir);
-			foreach (scandir($dir) as $item) {
-				if ($item == '.' || $item == '..') continue;
-				if (is_file($dir . '/' . $item)) {
-					if (unlink($dir . '/' . $item)) {
-					}
-				} elseif (is_dir($dir . '/' . $item)) {
-					if (!$this->delTree($dirRelative . "/" . $item)) {
-						return false;
-					};
-				}
-			}
-			if ($return = rmdir($dir)) {
-			}
-			return $return;
 		}
 
 		private static function getFileSizeFromOS($fullPath) {
