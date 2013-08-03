af1d5c048f434f75ae57fd29e0cb93eb065c7f3a
kondou <kondou@ts.unde.re>
2013-08-04 01:17:33 +0200

    Relative status.php, not always domain.tld/status.php

--- core/js/maintenance-check.js
+++ core/js/maintenance-check.js
@@ -3,7 +3,8 @@ var timer = window.setInterval(checkStatus, 20000);
 
 function checkStatus() {
 	var request = new XMLHttpRequest();
-	request.open("GET", '/status.php', true);
+	ocroot = location.pathname.substr(0, location.pathname.indexOf('index.php'));
+	request.open("GET", ocroot+'status.php', true);
 	request.send();
 	request.onreadystatechange = function() {
 		if (request.readyState === 4) {
