91ce35b70199075f23fb42a5ae71e6ad79395d24
Bjoern Schiessle <schiessle@owncloud.com>
2013-09-05 14:59:18 +0200

    don't auto-logout the user if the encryption app was enabled, instead just display the warning

--- apps/files_encryption/templates/invalid_private_key.php
+++ apps/files_encryption/templates/invalid_private_key.php
@@ -2,9 +2,9 @@
 	<li class='error'>
 		<?php $location = \OC_Helper::linkToRoute( "settings_personal" ).'#changePKPasswd' ?>
 
-		<?php p($l->t('Your private key is not valid! Maybe the your password was changed from outside.')); ?>
+		<?php p($l->t('Your private key is not valid  or wasn\'t initialized correctly! Maybe the your password was changed from outside. If your log-in password didn\'t change than first try to log out and log in back in order to initialize your encryption keys')); ?>
 		<br/>
-		<?php p($l->t('You can unlock your private key in your ')); ?> <a href="<?php echo $location?>"><?php p($l->t('personal settings')); ?>.</a>
+		<?php p($l->t('If this doesn\'t help you can unlock your private key in your ')); ?> <a href="<?php echo $location?>"><?php p($l->t('personal settings')); ?>.</a>
 		<br/>
 	</li>
 </ul>
