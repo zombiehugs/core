e8c421e593fad3598f6869cf5cc36e07354f588b
kondou <kondou@ts.unde.re>
2013-10-13 23:30:08 +0200

    Show eye always

--- core/js/js.js
+++ core/js/js.js
@@ -746,15 +746,8 @@ $(document).ready(function(){
 	});
 
 	var setShowPassword = function(input, label) {
-		input.showPassword().keyup(function(){
-			if (input.val().length == 0) {
-				label.hide();
-			}
-			else {
-				label.css("display", "inline").show();
-			}
-		});
-		label.hide();
+		label.css("display", "inline");
+		input.showPassword();
 	};
 	setShowPassword($('#adminpass'), $('label[for=show]'));
 	setShowPassword($('#pass2'), $('label[for=personal-show]'));
