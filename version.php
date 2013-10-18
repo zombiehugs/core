b4c2ffdf67731cff795b556c432795f2d519831f
Frank Karlitschek <frank@owncloud.org>
2013-10-18 14:34:15 +0200

    fixing my wrong commit 2 days ago. Sorry for the confusion.

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
 // We only can count up. The 4. digit is only for the internal patchlevel to trigger DB upgrades between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel when updating major/minor version number.
-$OC_Version=array(5, 00, 0, 1);
+$OC_Version=array(6, 00, 0, 1);
 
 // The human radable string
 $OC_VersionString='6.0 alpha 2';
