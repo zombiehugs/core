185216361b3c2f84f214c16e2304d9941d9105f4
ringmaster <epithet@gmail.com>
2013-10-21 12:10:39 -0400

    Test for 'avconv', not for 'which'.

--- lib/private/preview/movies.php
+++ lib/private/preview/movies.php
@@ -8,44 +8,47 @@
  */
 namespace OC\Preview;
 
-// movie preview is currently not supported on Windows
-if (!\OC_Util::runningOnWindows()) {
-	$isShellExecEnabled = !in_array('shell_exec', explode(', ', ini_get('disable_functions')));
-	$whichAVCONV = ($isShellExecEnabled ? shell_exec('which avconv') : '');
-	$isAVCONVAvailable = !empty($whichAVCONV);
+if (!isset(\OC::$session['isAVCONVAvailable']) || \OC::$session['isAVCONVAvailable'] == false) {
+	$disabledFunctions = explode(', ', ini_get('disable_functions'));
+	$neededFunctions = array('shell_exec', 'exec');
 
-	if($isShellExecEnabled && $isAVCONVAvailable) {
+	if (count(array_intersect($neededFunctions, $disabledFunctions)) == 0) {
+		exec('avconv', $output, $return);
+		\OC::$session['isAVCONVAvailable'] = count($output) > 0;
+	}
+}
 
-		class Movie extends Provider {
+if(\OC::$session['isAVCONVAvailable']) {
 
-			public function getMimeType() {
-				return '/video\/.*/';
-			}
+	class Movie extends Provider {
 
-			public function getThumbnail($path, $maxX, $maxY, $scalingup, $fileview) {
-				$absPath = \OC_Helper::tmpFile();
-				$tmpPath = \OC_Helper::tmpFile();
-
-				$handle = $fileview->fopen($path, 'rb');
-
-				$firstmb = stream_get_contents($handle, 1048576); //1024 * 1024 = 1048576
-				file_put_contents($absPath, $firstmb);
-
-				//$cmd = 'ffmpeg -y  -i ' . escapeshellarg($absPath) . ' -f mjpeg -vframes 1 -ss 1 -s ' . escapeshellarg($maxX) . 'x' . escapeshellarg($maxY) . ' ' . $tmpPath;
-				$cmd = 'avconv -an -y -ss 1 -i ' . escapeshellarg($absPath) . ' -f mjpeg -vframes 1 ' . escapeshellarg($tmpPath);
-
-				shell_exec($cmd);
-
-				$image = new \OC_Image($tmpPath);
-
-				unlink($absPath);
-				unlink($tmpPath);
-
-				return $image->valid() ? $image : false;
-			}
+		public function getMimeType() {
+			return '/video\/.*/';
 		}
 
-		\OC\Preview::registerProvider('OC\Preview\Movie');
+		public function getThumbnail($path, $maxX, $maxY, $scalingup, $fileview) {
+			$absPath = \OC_Helper::tmpFile();
+			$tmpPath = \OC_Helper::tmpFile();
+
+			$handle = $fileview->fopen($path, 'rb');
+
+			$firstmb = stream_get_contents($handle, 1048576); //1024 * 1024 = 1048576
+			file_put_contents($absPath, $firstmb);
+
+			//$cmd = 'ffmpeg -y  -i ' . escapeshellarg($absPath) . ' -f mjpeg -vframes 1 -ss 1 -s ' . escapeshellarg($maxX) . 'x' . escapeshellarg($maxY) . ' ' . $tmpPath;
+			$cmd = 'avconv -an -y -ss 1 -i ' . escapeshellarg($absPath) . ' -f mjpeg -vframes 1 ' . escapeshellarg($tmpPath);
+
+			shell_exec($cmd);
+
+			$image = new \OC_Image($tmpPath);
+
+			unlink($absPath);
+			unlink($tmpPath);
+
+			return $image->valid() ? $image : false;
+		}
 	}
+
+	\OC\Preview::registerProvider('OC\Preview\Movie');
 }
 
