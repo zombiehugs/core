6e38036142771d9fe0ca810578bda7d3e1c08400
raghunayyar <me@iraghu.com>
2013-11-05 01:59:57 +0530

    Adds angular-mocks to vendor for tests.

--- settings/tests/apps/js/config/karma.js
+++ settings/tests/apps/js/config/karma.js
@@ -28,6 +28,7 @@ module.exports = function(config) {
 		files: [
 			'js/vendor/angular/angular.js',
 			'js/vendor/angular-resource/angular-resource.js',
+			'js/vendor/angular-mocks/angular-mocks.js',			
 			'js/apps/config/config.js',
 			'js/apps/app/**/*.js',
 			'tests/apps/js/unit/**/*.js'
